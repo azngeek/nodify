5711896b0b2159c1ae3c2d949c584b6b48094d18
Don Bosco van Hoi <donbosco.rulz@gmail.com>
2013-02-14 18:03:33 +0100

    added basic routing

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,7 +1,23 @@
-/**
- * Created with JetBrains PhpStorm.
- * User: donbosco
- * Date: 14.02.13
- * Time: 17:38
- * To change this template use File | Settings | File Templates.
- */
+// routing based on http://backbonetutorials.com/what-is-a-router/#/route/action
+// https://github.com/grifotv/grifotv-portfolio/tree/master/bin <- Killer portfolio
+// http://www.azngeek.de:3000/backbone#/postasdfa/asfda22
+
+var AppRouter = Backbone.Router.extend(
+    {
+        routes: {
+            "user/:id": "getUser",
+            "*actions": "defaultRoute" // matches http://example.com/#anything-here
+        }
+    }
+);
+app_router = new AppRouter;
+
+app_router.on('route:defaultRoute', function(actions) {
+   // alert(actions);
+});
+
+app_router.on('route:getUser', function(id) {
+    console.log('id was: ' + id);
+});
+
+Backbone.history.start();
